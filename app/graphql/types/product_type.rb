fa4290daa98e361bea954114b0dba2e0963b45f0
Nick Yang <nick.yang3721@gmail.com>
2019-01-13 22:01:55 -0500

    Add Product Type

---
+++ app/graphql/types/product_type.rb
@@ -0,0 +1,8 @@
+ProductType = GraphQL::ObjectType.define do
+ name "Product"
+  description "A product in an online marketplace"
+  field :id, types.ID
+  field :title, types.String
+  field :price, types.Float
+  field :inventory_count, types.Int
+end
