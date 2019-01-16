395095829787e66b84fe65722dc919aaf997f97f
Nick Yang <nick.yang3721@gmail.com>
2019-01-15 22:01:38 -0500

    Update product query with available products argument

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,12 +3,14 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   # queries are just represented as fields
   field :allProducts, !types[Types::ProductType] do
+    argument :onlyAvailableProducts, types.Boolean, default_value: false
     # resolve would be called in order to fetch data for that field
-    resolve -> (obj, args, ctx) { Product.all }
-  end
-
-  field :allavailableProducts, !types[Types::ProductType] do
-    # resolve would be called in order to fetch data for that field
-    resolve -> (obj, args, ctx) { Product.where('inventory_count > 0') }
+    resolve -> (obj, args, ctx) { 
+        if args[:onlyAvailableProducts]
+          Product.where('inventory_count > 0')
+        else 
+          Product.all
+        end
+       }
   end
 end
