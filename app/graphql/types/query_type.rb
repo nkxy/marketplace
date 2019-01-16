6a65fe3ea64c7745f3be9fd5271b3568cfd79a6c
Nick Yang <nick.yang3721@gmail.com>
2019-01-16 18:38:01 -0500

    Add error handling for product queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,11 +6,15 @@ Types::QueryType = GraphQL::ObjectType.define do
     argument :onlyAvailableProducts, types.Boolean, default_value: false
     # resolve would be called in order to fetch data for that field
     resolve -> (obj, args, ctx) { 
+      begin
         if args[:onlyAvailableProducts]
           Product.where('inventory_count > 0')
         else 
           Product.all
         end
        }
+      rescue 
+        raise GraphQL::ExecutionError.new("Unable to fetch products.")
+      end
   end
 end
