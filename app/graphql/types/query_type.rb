41db7a756946904daa159cd3609504c0020647f7
Nick Yang <nick.yang3721@gmail.com>
2019-01-15 21:44:31 -0500

    Add seperate query for only available products

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,4 +6,9 @@ Types::QueryType = GraphQL::ObjectType.define do
     # resolve would be called in order to fetch data for that field
     resolve -> (obj, args, ctx) { Product.all }
   end
+
+  field :allavailableProducts, !types[Types::ProductType] do
+    # resolve would be called in order to fetch data for that field
+    resolve -> (obj, args, ctx) { Product.where('inventory_count > 0') }
+  end
 end
