f5a00ed07b9a0d2c4687e14abfcc54ab4d22c8e3
Nick Yang <nick.yang3721@gmail.com>
2019-01-16 18:27:31 -0500

    Add error handling for purchasing products

--- app/graphql/resolvers/purchase_product.rb
+++ app/graphql/resolvers/purchase_product.rb
@@ -6,10 +6,15 @@ class Resolvers::PurchaseProduct < GraphQL::Function
   type Types::ProductType
 
   def call(_obj, args, _ctx)
-    item = Product.find_by_title(args[:title])
-    if item.inventory_count > 0 
-      item.update inventory_count: item.inventory_count - 1
+    if item = Product.find_by_title(args[:title])
+      if item.inventory_count > 0 
+        item.update inventory_count: item.inventory_count - 1
+      else 
+        raise GraphQL::ExecutionError.new("#{args[:title]} is out of stock.")
+      end
+      Product.find_by_title(args[:title])
+    else 
+      raise GraphQL::ExecutionError.new("#{args[:title]} does not exist.")
     end
-    Product.find_by_title(args[:title])
   end
 end
