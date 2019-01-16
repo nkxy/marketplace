ea7f54e8ad3936adc1abf0f9167e9834e0a29cd3
Nick Yang <nick.yang3721@gmail.com>
2019-01-16 18:38:50 -0500

    Update seeds to clear database first

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Product.destroy_all
+
 Product.create!([
   {
     title: "Headphones",
