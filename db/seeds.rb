7b7f11b291852117fa623019fc4eeaefa86b35d6
Nick Yang <nick.yang3721@gmail.com>
2019-01-16 18:38:12 -0500

    Add seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!([
+  {
+    title: "Headphones",
+    price: 12.99,
+    inventory_count: 24
+  },
+  {
+    title: "Ukulele",
+    price: 80.99,
+    inventory_count: 1
+  },
+  {
+    title: "Ruby Necklace",
+    price: 499.99,
+    inventory_count: 0
+  },
+  {
+    title: "Laptop Case",
+    price: 19.99,
+    inventory_count: 100
+  },
+  {
+    title: "The Life-Changing Magic of Tidying Up: The Japanese Art of Decluttering and Organizing",
+    price: 10.99,
+    inventory_count: 0
+  },
+])
